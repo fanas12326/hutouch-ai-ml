46326cdded1bd7869e3317b80afd916f8511889e
kshitij-gavhane <gavhanekshitij8@gmail.com>
2024-05-31 16:38:51 +0530

    new

--- main.py
+++ main.py
@@ -989,7 +989,6 @@ async def figma_custom_ui(item: Item):
     print(api_json_data)
     
     openai_api_key = os.getenv('OPENAI_API_KEY')
-    print('Open ai api key: '+openai_api_key)
     client = OpenAI(api_key=openai_api_key)
 
     #Prompt 1
